c9c343ee281a53c2360c256faf0a395aec1aae40
Hasan Al Mamun <hihasan07@gmail.com>
2025-05-02 23:35:46 +0600

    Iniital Commit

---
+++ src/main/kotlin/xyz/hihasan/finance/FinanceAppApplication.kt
@@ -0,0 +1,11 @@
+package xyz.hihasan.finance
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FinanceAppApplication
+
+fun main(args: Array<String>) {
+	runApplication<FinanceAppApplication>(*args)
+}
---
+++ src/test/kotlin/xyz/hihasan/finance/FinanceAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package xyz.hihasan.finance
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FinanceAppApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
